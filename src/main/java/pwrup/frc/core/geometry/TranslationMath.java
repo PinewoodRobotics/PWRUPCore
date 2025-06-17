632035d2e8a6135f9cf3db5787571fcb794ef88c
brigerodev <golden.notch.creeper@gmail.com>
2025-06-17 12:29:01 -0700

    remove some stuff and add deadband

--- src/main/java/pwrup/frc/core/geometry/TranslationMath.java
+++ src/main/java/pwrup/frc/core/geometry/TranslationMath.java
@@ -12,4 +12,32 @@ public class TranslationMath {
       translation.getY()
     );
   }
+
+  /**
+   * Deadbands joystick input, then scales it from the deadband to 1. Ask Jared
+   * for clarification.
+   *
+   * @param input    the joystick input, [0, 1]
+   * @param deadband ignores the input if it is less than this value, [0, 1]
+   * @param minValue adds this value if the input overcomes the deadband, [0, 1]
+   * @return the return value, [0, 1]
+   */
+  public static double deadband(
+    double input,
+    double deadband,
+    double minValue
+  ) {
+    double output;
+    double m = (1.0 - minValue) / (1.0 - deadband);
+
+    if (Math.abs(input) < deadband) {
+      output = 0;
+    } else if (input > 0) {
+      output = m * (input - deadband) + minValue;
+    } else {
+      output = m * (input + deadband) - minValue;
+    }
+
+    return output;
+  }
 }
