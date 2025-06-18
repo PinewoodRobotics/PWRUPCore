0b0b71506878565211d6990749453f59a716c848
brigerodev <golden.notch.creeper@gmail.com>
2025-06-17 17:38:57 -0700

    add wrap

--- src/main/java/pwrup/frc/core/geometry/RotationMath.java
+++ src/main/java/pwrup/frc/core/geometry/RotationMath.java
@@ -1,3 +1,13 @@
 package pwrup.frc.core.geometry;
 
-public class RotationMath {}
+public class RotationMath {
+
+  public static double wrapTo180(double angle) {
+    double newAngle = (angle + 180) % 360;
+    while (newAngle < 0) {
+      newAngle += 360;
+    }
+
+    return newAngle - 180;
+  }
+}
