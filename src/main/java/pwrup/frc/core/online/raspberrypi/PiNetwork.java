07a7d7704adb789627b8fc863e76356237c822f0
brigerodev <golden.notch.creeper@gmail.com>
2025-08-21 22:43:44 -0700

    autobahn client integration and add newer verson

--- src/main/java/pwrup/frc/core/online/raspberrypi/PiNetwork.java
+++ src/main/java/pwrup/frc/core/online/raspberrypi/PiNetwork.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import autobahn.client.Address;
+
 public class PiNetwork<P extends Enum<P>> {
 
     private final List<RaspberryPi<P>> raspberryPis;
@@ -28,6 +30,18 @@ public class PiNetwork<P extends Enum<P>> {
         add(new RaspberryPi<P>(address, address, Arrays.asList(processesToRun)));
     }
 
+    public void add(Address address, String name, P... processesToRun) {
+        add(new RaspberryPi<P>(toAddressString(address), name, Arrays.asList(processesToRun)));
+    }
+
+    public void add(Address address, P... processesToRun) {
+        add(new RaspberryPi<P>(toAddressString(address), address.toString(), Arrays.asList(processesToRun)));
+    }
+
+    private String toAddressString(Address address) {
+        return "http://" + address.getHost() + ":" + address.getPort();
+    }
+
     public boolean startAllPis() {
         return raspberryPis.stream().allMatch(RaspberryPi::startProcesses);
     }
@@ -38,5 +52,9 @@ public class PiNetwork<P extends Enum<P>> {
 
     public boolean setConfig(String rawConfig) {
         return raspberryPis.stream().allMatch(raspberryPi -> raspberryPi.setConfig(rawConfig));
+    }
+
+    public boolean restartAllPis() {
+        return stopAllPis() && startAllPis();
     }
 }
